2391b3456c7456c46f68fba1ef18e85724e95421
Knock <enoch10evelyn@gmail.com>
2022-07-23 16:07:14 -0700

    Added an ID to the form element and targeted the ID via creating a const of form and added a eventlister with submit. Will attach function linked to api

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@
 const aboutUsButton = document.querySelector('#aboutUs');
 const modalBg = document.querySelector('.modal-background');
 const modal = document.querySelector('.modal');
+const form = document.querySelector('#form');
 
 aboutUsButton.addEventListener('click', () => {
     modal.classList.add('is-active');
@@ -10,5 +11,7 @@ aboutUsButton.addEventListener('click', () => {
 modalBg.addEventListener('click', () => {
     modal.classList.remove('is-active');
 })
+//Function that is triggered via click or clicking enter will be placed before the search event listener 
+//form.addEventListener('submit', ){
 
-
+//};
