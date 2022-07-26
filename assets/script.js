532493769e9fa5763d5df084dfe5d2afbab2407b
callahn mcdonald <callahn@gmail.com>
2022-07-25 20:57:43 -0600

    applied api key for emojifinder and polished up a few other things

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,34 @@
 // modal
-const emoji1 = document.getElementById("1");
-const emoji2 = document.getElementById("2");
-const emoji3 = document.getElementById("3");
 const aboutUsButton = document.querySelector('#aboutUs');
 const modalBg = document.querySelector('.modal-background');
 const modal = document.querySelector('.modal');
 const form = document.querySelector('#form');
-function updateEmoji(element) {
-    fetch(
-        "https://emojihub.herokuapp.com/api/random/category_smileys_and_people"
-    )
-        .then((response) => response.json())
-        .then((emoji) => {
-            element.innerHTML = emoji.htmlCode[0];
-        });
-
+const searchButton = document.querySelector('#search');
+const searchText = document.querySelector('#search-text');
 aboutUsButton.addEventListener('click', () => {
     modal.classList.add('is-active');
-})
+});
 
 modalBg.addEventListener('click', () => {
     modal.classList.remove('is-active');
-})
-//Function that is triggered via click or clicking enter will be placed before the search event listener 
-//form.addEventListener('submit', ){
+});
 
-//};
+
+let emojiCache = [];
+
+searchButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (!emojiCache.length) {
+        fetch("https://emojihub.herokuapp.com/api/all")
+            .then((response) => response.json())
+            .then((data) => {
+                emojiCache = data;
+            });
+    }
+    const result = emojiCache.find((emoji) => {
+         return emoji.name.startsWith(searchText.value.toLowerCase().trim());
+    });
+    document.getElementById("emoji-results").innerHTML=result?.htmlCode
+        ? result?.htmlCode
+        : "not found";
+});
